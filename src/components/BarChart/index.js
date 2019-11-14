c4808a680e223279a6e42e8c78d6c5af7d921155
seta-namle <nam.le@seta-international.vn>
2019-11-14 17:22:54 +0700

    update taskS

--- src/components/BarChart/index.js
+++ src/components/BarChart/index.js
@@ -3,40 +3,33 @@ import {
     BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, Line, ComposedChart
 } from 'recharts';
 import { Card } from 'antd';
+import PropTypes from 'prop-types';
 import styles from './styles.scss';
-class BarChartComponent extends Component {
+const BarChartComponent = ({data}) => (
+    <Card className={styles['card-chart']}>
+     <ResponsiveContainer width="100%" height={350} >
+        <ComposedChart  data={data}
+            margin={{ top: 20, right: 30, left: 20, bottom: 5 }}>
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Bar dataKey="Running" stackId="a" fill="#8884d8" />
+            <Bar dataKey="Started" stackId="a" fill="#82ca9d" />
+            <Bar dataKey="Paused" stackId="a" fill="#ef7c4d" />
 
-    render() {
-        const data = [
-            { name: 'Page A', uv: 4000, pv: 2400, amt: 2400, categoryId: 10 },
-            { name: 'Page B', uv: 3000, pv: 1398, amt: 2210, categoryId: 10 },
-            { name: 'Page C', uv: 2000, pv: 9800, amt: 2290, categoryId: 10 },
-            { name: 'Page D', uv: 2780, pv: 3908, amt: 2000, categoryId: 10 },
-            { name: 'Page E', uv: 1890, pv: 4800, amt: 2181, categoryId: 10 },
-            { name: 'Page F', uv: 2390, pv: 3800, amt: 2500, categoryId: 10 },
-            { name: 'Page G', uv: 3490, pv: 4300, amt: 2100, categoryId: 10 },
-        ];
-        return (
-            <Card className={styles['card-chart']}>
-             <ResponsiveContainer width="100%" height={350} >
-                <ComposedChart  data={data}
-                    margin={{ top: 20, right: 30, left: 20, bottom: 5 }}>
-                    <CartesianGrid strokeDasharray="3 3" />
-                    <XAxis dataKey="name" />
-                    <YAxis />
-                    <Tooltip />
-                    <Legend />
-                    <Bar dataKey="pv" stackId="a" fill="#8884d8" />
-                    <Bar dataKey="uv" stackId="a" fill="#82ca9d" />
-                    <Bar dataKey="amt" stackId="a" fill="#ef7c4d" />
+            <Line type='monotone' dataKey='Error rate' stroke='#ff7300' />
+        </ComposedChart>
+        </ResponsiveContainer>
+    </Card>
 
-                    <Line type='monotone' dataKey='uv' stroke='#ff7300' />
-                </ComposedChart>
-                </ResponsiveContainer>
-            </Card>
+)
+BarChartComponent.propTypes = {
+    data: PropTypes.arrayOf(
+        PropTypes.shape({
 
-        )
-    }
+        })
+    )
 }
-
 export default BarChartComponent;
--- src/components/Tasks/index.js
+++ src/components/Tasks/index.js
@@ -19,7 +19,88 @@ class Tasks extends Component {
   };
   
   state = {
+      filterBarChart: 'all',
+      dataBarChart: [
+        { name: '01:00 AM', Running: 4000, Started: 2400, Paused: 2400, 'Error rate': 2400, key: 'all' },
+        { name: '02:00 AM', Running: 3000, Started: 1398, Paused: 2210, 'Error rate': 1200, key: 'all' },
+        { name: '03:00 AM', Running: 2000, Started: 9800, Paused: 2290, 'Error rate': 2000, key: 'all' },
+        { name: '04:00 AM', Running: 2780, Started: 3908, Paused: 2000, 'Error rate': 3000, key: 'all' },
+        { name: '05:00 AM', Running: 1890, Started: 4800, Paused: 2181, 'Error rate': 2200, key: 'all' },
+        { name: '06:00 AM', Running: 2390, Started: 3800, Paused: 2500, 'Error rate': 1000, key: 'all' },
+        { name: '07:00 AM', Running: 3490, Started: 4300, Paused: 2100, 'Error rate': 1200, key: 'all' },
+        { name: '08:00 AM', Running: 3490, Started: 4300, Paused: 2100, 'Error rate': 1200, key: 'all' },
+        { name: '09:00 AM', Running: 3490, Started: 4300, Paused: 2100, 'Error rate': 1200, key: 'all' },
+        { name: '10:00 AM', Running: 3490, Started: 4300, Paused: 2100, 'Error rate': 1200, key: 'all' },
+        { name: '11:00 AM', Running: 3490, Started: 4300, Paused: 2100, 'Error rate': 1200, key: 'all' },
+        { name: '12:00 AM', Running: 3490, Started: 4300, Paused: 2100, 'Error rate': 1200, key: 'all' },
 
+
+        { name: '01:00 AM', Running: 800, Started: 300, Paused: 300, 'Error rate': 800, key: 'translation' },
+        { name: '02:00 AM', Running: 600, Started: 200, Paused: 500, 'Error rate': 400, key: 'translation' },
+        { name: '03:00 AM', Running: 500, Started: 1800, Paused: 500, 'Error rate': 300, key: 'translation' },
+        { name: '04:00 AM', Running: 580, Started: 780, Paused: 500, 'Error rate': 200, key: 'translation' },
+        { name: '05:00 AM', Running: 300, Started: 800, Paused: 200, 'Error rate': 100, key: 'translation' },
+        { name: '06:00 AM', Running: 320, Started: 300, Paused: 220, 'Error rate': 100, key: 'translation' },
+        { name: '07:00 AM', Running: 200, Started: 820, Paused: 220, 'Error rate': 200, key: 'translation' },
+        { name: '08:00 AM', Running: 3490, Started: 4300, Paused: 2100, 'Error rate': 1200, key: 'translation' },
+        { name: '09:00 AM', Running: 3490, Started: 4300, Paused: 2100, 'Error rate': 1200, key: 'translation' },
+        { name: '10:00 AM', Running: 3490, Started: 4300, Paused: 2100, 'Error rate': 1200, key: 'translation' },
+        { name: '11:00 AM', Running: 3490, Started: 4300, Paused: 2100, 'Error rate': 1200, key: 'translation' },
+        { name: '12:00 AM', Running: 3490, Started: 4300, Paused: 2100, 'Error rate': 1200, key: 'translation' },
+
+        { name: '01:00 AM', Running: 680, Started: 300, Paused: 300, 'Error rate': 120, key: 'faceDetection' },
+        { name: '02:00 AM', Running: 580, Started: 200, Paused: 500, 'Error rate': 220, key: 'faceDetection' },
+        { name: '03:00 AM', Running: 460, Started: 1800, Paused: 500, 'Error rate': 200, key: 'faceDetection' },
+        { name: '04:00 AM', Running: 580, Started: 780, Paused: 500, 'Error rate': 300, key: 'faceDetection' },
+        { name: '05:00 AM', Running: 300, Started: 800, Paused: 200, 'Error rate': 320, key: 'faceDetection' },
+        { name: '06:00 AM', Running: 320, Started: 300, Paused: 220, 'Error rate': 120, key: 'faceDetection' },
+        { name: '07:00 AM', Running: 200, Started: 820, Paused: 220, 'Error rate': 100, key: 'faceDetection' },
+        { name: '08:00 AM', Running: 3490, Started: 4300, Paused: 2100, 'Error rate': 1200, key: 'faceDetection' },
+        { name: '09:00 AM', Running: 3490, Started: 4300, Paused: 2100, 'Error rate': 1200, key: 'faceDetection' },
+        { name: '10:00 AM', Running: 3490, Started: 4300, Paused: 2100, 'Error rate': 1200, key: 'faceDetection' },
+        { name: '11:00 AM', Running: 3490, Started: 4300, Paused: 2100, 'Error rate': 1200, key: 'faceDetection' },
+        { name: '12:00 AM', Running: 3490, Started: 4300, Paused: 2100, 'Error rate': 1200, key: 'faceDetection' },
+
+        { name: '01:00 AM', Running: 380, Started: 200, Paused: 200, 'Error rate': 220, key: 'transcription' },
+        { name: '02:00 AM', Running: 240, Started: 200, Paused: 180, 'Error rate': 120, key: 'transcription' },
+        { name: '03:00 AM', Running: 180, Started: 800, Paused: 280, 'Error rate': 210, key: 'transcription' },
+        { name: '04:00 AM', Running: 180, Started: 200, Paused: 200, 'Error rate': 180, key: 'transcription' },
+        { name: '05:00 AM', Running: 300, Started: 220, Paused: 200, 'Error rate': 140, key: 'transcription' },
+        { name: '06:00 AM', Running: 120, Started: 200, Paused: 100, 'Error rate': 420, key: 'transcription' },
+        { name: '07:00 AM', Running: 180, Started: 180, Paused: 200, 'Error rate': 180, key: 'transcription' },
+        { name: '08:00 AM', Running: 3490, Started: 4300, Paused: 2100, 'Error rate': 1200, key: 'transcription' },
+        { name: '09:00 AM', Running: 3490, Started: 4300, Paused: 2100, 'Error rate': 1200, key: 'transcription' },
+        { name: '10:00 AM', Running: 3490, Started: 4300, Paused: 2100, 'Error rate': 1200, key: 'transcription' },
+        { name: '11:00 AM', Running: 3490, Started: 4300, Paused: 2100, 'Error rate': 1200, key: 'transcription' },
+        { name: '12:00 AM', Running: 3490, Started: 4300, Paused: 2100, 'Error rate': 1200, key: 'transcription' },
+
+        { name: '01:00 AM', Running: 200, Started: 100, Paused: 200, 'Error rate': 100, key: 'logoRecognition' },
+        { name: '02:00 AM', Running: 80, Started: 60, Paused: 80, 'Error rate': 120, key: 'logoRecognition' },
+        { name: '03:00 AM', Running: 60, Started: 80, Paused: 98, 'Error rate': 240, key: 'logoRecognition' },
+        { name: '04:00 AM', Running: 180, Started: 80, Paused: 200, 'Error rate': 210, key: 'logoRecognition' },
+        { name: '05:00 AM', Running: 110, Started: 220, Paused: 80, 'Error rate': 260, key: 'logoRecognition' },
+        { name: '06:00 AM', Running: 180, Started: 100, Paused: 30, 'Error rate': 200, key: 'logoRecognition' },
+        { name: '07:00 AM', Running: 100, Started: 120, Paused: 200, 'Error rate': 320, key: 'logoRecognition' },
+        { name: '08:00 AM', Running: 3490, Started: 4300, Paused: 2100, 'Error rate': 1200, key: 'logoRecognition' },
+        { name: '09:00 AM', Running: 3490, Started: 4300, Paused: 2100, 'Error rate': 1200, key: 'logoRecognition' },
+        { name: '10:00 AM', Running: 3490, Started: 4300, Paused: 2100, 'Error rate': 1200, key: 'logoRecognition' },
+        { name: '11:00 AM', Running: 3490, Started: 4300, Paused: 2100, 'Error rate': 1200, key: 'logoRecognition' },
+        { name: '12:00 AM', Running: 3490, Started: 4300, Paused: 2100, 'Error rate': 1200, key: 'logoRecognition' },
+
+        { name: '01:00 AM', Running: 2000, Started: 1200, Paused: 1200, 'Error rate': 420, key: 'licensePlate' },
+        { name: '02:00 AM', Running: 600, Started: 800, Paused: 1000, 'Error rate': 260, key: 'licensePlate' },
+        { name: '03:00 AM', Running: 600, Started: 3000, Paused: 1000, 'Error rate': 220, key: 'licensePlate' },
+        { name: '04:00 AM', Running: 1400, Started: 1800, Paused: 1000, 'Error rate': 200, key: 'licensePlate' },
+        { name: '05:00 AM', Running: 1600, Started: 2200, Paused: 1000, 'Error rate': 120, key: 'licensePlate' },
+        { name: '06:00 AM', Running: 1800, Started: 1800, Paused: 2200, 'Error rate': 100, key: 'licensePlate' },
+        { name: '07:00 AM', Running: 3200, Started: 2800, Paused: 2100, 'Error rate': 80, key: 'licensePlate' },
+        { name: '08:00 AM', Running: 3490, Started: 4300, Paused: 2100, 'Error rate': 1200, key: 'licensePlate' },
+        { name: '09:00 AM', Running: 3490, Started: 4300, Paused: 2100, 'Error rate': 1200, key: 'licensePlate' },
+        { name: '10:00 AM', Running: 3490, Started: 4300, Paused: 2100, 'Error rate': 1200, key: 'licensePlate' },
+        { name: '11:00 AM', Running: 3490, Started: 4300, Paused: 2100, 'Error rate': 1200, key: 'licensePlate' },
+        { name: '12:00 AM', Running: 3490, Started: 4300, Paused: 2100, 'Error rate': 1200, key: 'licensePlate' },
+      ],
+      activeIndex: null
   }
   onClickRow = value => {
     const { onClickDetail, tabName } = this.props;
@@ -32,8 +113,27 @@ class Tasks extends Component {
   onClickPieChart = (data, index) => {
     console.log('data', data)
     console.log('index', index)
-
+    const { activeIndex } = this.state;
+    const filterBarChart = data.payload.payload.key
+    if(activeIndex || activeIndex === 0){
+      this.setState({
+        filterBarChart: 'all',
+        activeIndex: null
+      })
+    }else{
+      this.setState({
+        filterBarChart,
+        activeIndex: index
+      })
+    }
+   
   }
+  // onPieEnter = (data, index) => {
+  //   console.log(index)
+  //   this.setState({
+  //     activeIndex: index
+  //   })
+  // }
   render() {
     const columns = [
       {
@@ -120,6 +220,11 @@ class Tasks extends Component {
     //   active: 40000,
     //   running: 2
     // }
+
+    const {
+      dataBarChart,
+      filterBarChart
+    } = this.state;
     return (
       <Fragment>
        
@@ -158,8 +263,10 @@ class Tasks extends Component {
               <PieChart
                 data={dataPieChart}
                 colors={colors}
-                
                 title={`Active tasks by engine category`}
+                onClick={this.onClickPieChart}
+                onPieEnter={this.onPieEnter} 
+                activeIndex={this.state.activeIndex}
               />
             </Col>
             <Col span={8}>
@@ -167,7 +274,6 @@ class Tasks extends Component {
                 data={dataPieChart}
                 colors={colors}
                 title={`Completed tasks by engine category`}
-                onClick={this.onClickPieChart}
               />
             </Col>
             <Col span={8}>
@@ -179,7 +285,7 @@ class Tasks extends Component {
             </Col>
           </Row>
        
-        <BarChart />
+        <BarChart data={dataBarChart.filter(item => item.key === filterBarChart)} />
         {/* TASKS
         <Table
           rowSelection={rowSelection}
