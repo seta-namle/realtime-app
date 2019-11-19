aed16653405d4e1f74d29cc4caf4a05c644803fb
seta-adam <nguyennam96.bill@gmail.com>
2019-11-19 16:01:31 +0700

    responsive job, table list

--- src/components/TableList/index.js
+++ src/components/TableList/index.js
@@ -5,36 +5,29 @@ import PropTypes from 'prop-types';
 import { ON_CLICK_DETAIL } from '../../state/modules/sideBar';
 import { selectCurrentRoutePayload } from 'state/modules/routing';
 const TableList = ({ columns, dataRow, title }) => (
-    <Card
-        title={title}
-        bordered={false}
-        style={{ marginTop: '10px' }}
-    >
-        <Table
-            pagination={{ pageSize: 10 }}
-            columns={columns}
-            dataSource={dataRow}
-        />
-    </Card>
-)
+  <Card title={title} bordered={false} style={{ marginTop: '10px' }}>
+    <Table
+      pagination={{ pageSize: 10 }}
+      columns={columns}
+      dataSource={dataRow}
+      scroll={{ x: 1500 }}
+    />
+  </Card>
+);
 TableList.propTypes = {
-    columns: PropTypes.arrayOf(
-        PropTypes.shape({})
-    ),
-    dataRow: PropTypes.arrayOf(
-        PropTypes.shape({})
-    ),
-    title: PropTypes.string
-}
+  columns: PropTypes.arrayOf(PropTypes.shape({})),
+  dataRow: PropTypes.arrayOf(PropTypes.shape({})),
+  title: PropTypes.string
+};
 export default connect(
-    state => ({
-        tabName: selectCurrentRoutePayload(state).tabName,
-        taskId: selectCurrentRoutePayload(state).id
-    }),
-    {
-        onClickDetail: payload => ({
-            type: ON_CLICK_DETAIL,
-            payload
-        })
-    }
+  state => ({
+    tabName: selectCurrentRoutePayload(state).tabName,
+    taskId: selectCurrentRoutePayload(state).id
+  }),
+  {
+    onClickDetail: payload => ({
+      type: ON_CLICK_DETAIL,
+      payload
+    })
+  }
 )(TableList);
