ecbbef4c2a2eade788b0914202e13b7e8b110edb
seta-namle <nam.le@setacinq.vn>
2019-11-20 16:41:30 +0700

    Resolve conflicts

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { render } from 'react-dom';
 import { handleImplicitRedirect } from 'veritone-oauth-helpers';
-
 import validateAppConfig from '~helpers/validateAppConfig';
 import configureStore from 'state/configureStore';
 
